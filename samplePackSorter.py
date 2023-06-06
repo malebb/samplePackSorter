36b40f60a865d4188505d08116634a91c4d47bfb
mlebrun <matthieulebrun55@gmail.com>
2023-06-06 23:35:10 +0200

    feat: copy each sample into the right category

--- samplePackSorter.py
+++ samplePackSorter.py
@@ -1,7 +1,7 @@
 import os;
 import argparse
 from argparse import RawTextHelpFormatter
-
+import shutil
 from enum import Enum
 
 class SampleTypes(Enum):
@@ -13,24 +13,41 @@ class SampleTypes(Enum):
     PERC = 5
     FX = 6
     BELL = 7
-    NONE = 8
+    OTHER = 8
 
+# Parse program's arguments
 parser = argparse.ArgumentParser(description="This program helps you sort your sample pack.\nIt uses the sample names to perform the sorting.", formatter_class=RawTextHelpFormatter)
 parser.add_argument("path", help="Path toward the directory containing your samples.")
 parser.add_argument("dest", help="Path where the sorted sample pack will be added.")
-
 parser.add_argument("name", help="Name of the created sample pack after sorting.")
-
 args = parser.parse_args()
 
-def getSampleType(sample):
-    for sampleTypeId in range(len(SampleTypes) - 1):
-        sampleType = SampleTypes(sampleTypeId).name.lower()
-        if (sample.lower().find(sampleType) != -1):
-            return SampleTypes(sampleTypeId)
-    return SampleTypes.NONE
+# Create sorted sample pack folder
+path_to_sorted_pack = os.path.join(args.dest, args.name)
+if (not os.path.exists(path_to_sorted_pack)):
+    os.mkdir(path_to_sorted_pack)
 
-for root, dirs, samples in os.walk(args.path, topdown=True):
-    for sample in samples:
-        sampleType = getSampleType(sample);
-        print(sampleType," " , sample);
+# Look for a category in the sample name
+def get_sample_type(sample):
+    for sample_type_id in range(len(SampleTypes) - 1):
+        sample_type = SampleTypes(sample_type_id).name.lower()
+        if (sample.lower().find(sample_type) != -1):
+            return SampleTypes(sample_type_id)
+    return SampleTypes.OTHER
+
+# Create a new sample category folder if never created
+def create_categorie_dir(sample_categorie):
+    categorie_path = os.path.join(path_to_sorted_pack, sample_categorie.name.lower())
+    if (not os.path.exists(categorie_path) and sample_categorie):
+        os.mkdir(categorie_path)
+    return (categorie_path)
+
+# Loop through each sample to sort them into the right category
+def sort_sample_pack():
+    for root, dirs, samples in os.walk(args.path, topdown=True):
+        for sample in samples:
+            sample_type = get_sample_type(sample);
+            sample_dest = create_categorie_dir(sample_type)
+            shutil.copy(os.path.join(root, sample), sample_dest)
+
+sort_sample_pack()
